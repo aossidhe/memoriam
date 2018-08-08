3b6853f8a6212c1b95e30401ba65e300881643fd
Cole Adams <atomsboys@gmail.com>
2018-08-08 13:11:50 -0500

    Added deletion detector, pivoted to checking playlist master page

--- background.js
+++ background.js
@@ -1,18 +1,20 @@
 /* Regex-pattern to check URLs against.
    It matches URLs like: http[s]://[...]stackoverflow.com[...] */
-var urlRegex = /^file:\/\/\/:?/;
+var urlRegex = /https?\:\/\/www\.youtube.com\/playlist\?list\=/;
 
 /* A function creator for callbacks */
 function doStuffWithDOM(element) {
-    alert("Discovered the following playlist name:\n" + element);
+    alert('Found: ' + element);
 }
 
 /* When the browser-action button is clicked... */
 chrome.browserAction.onClicked.addListener(function(tab) {
+    console.log("Extension triggered.");
     /*...check the URL of the active tab against our pattern and... */
-    //if (urlRegex.test(tab.url)) {
+    if (urlRegex.test(tab.url)) {
         /* ...if it matches, send a message specifying a callback too */
         chrome.tabs.sendMessage(tab.id, { text: "report_back" },
                                 doStuffWithDOM);
-    //}
+    }
+    console.log("Background completed.")
 });
--- content.js
+++ content.js
@@ -1,10 +1,22 @@
 /* Listen for messages */
 chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
-    /* If the received message has the expected format... */
-    if (msg.text && (msg.text == "report_back")) {
-        /* Call the specified callback, passing
-           the web-pages DOM content as argument */
-    console.log(document.getElementById('header-description').firstChild.firstChild.innerText);
-    sendResponse(document.getElementById('header-description').firstChild.firstChild.innerText);
+  /* If the received message has the expected format... */
+  if (msg.text && (msg.text == "report_back")) {
+    /* Call the specified callback, passing
+       the web-pages DOM content as argument */
+    userID = "UCInsZlvlbBVXDbTXYMkdNtg";
+    deleteMessage = "One or more videos have been removed from the playlist because they were deleted from YouTube."
+    if(document.getElementById("owner-name").firstChild.href.toString().includes(userID)){
+      console.log("This is a personal playlist.");
+      //console.log(document.querySelector("#container > yt-formatted-string").innerHTML.toString());
+      if(document.querySelector("#container > yt-formatted-string").innerHTML.toString().includes(deleteMessage)) {
+        console.log("Deletions detected.");
+      }
     }
+    else console.log("This is a foreign playlist.");
+
+    //console.log(document.getElementById("title").firstChild.href)
+    //console.log(document.querySelector('#title > a').innerText);
+    sendResponse(document.getElementById("title").firstChild.innerText);
+  }
 });
